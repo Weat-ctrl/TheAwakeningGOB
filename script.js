1486cfb059c6ab9ee3fc7253abb1319a78a1a1d0
Weat-ctrl <aaaaaaarrrrrrrgghhh@gmail.com>
2025-07-14 12:09:15 +0300

    Update script.js v160?

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 
 // --- Scene Setup ---
 const scene = new THREE.Scene();
