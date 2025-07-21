732d7db8306f22618fda59d38e67c3fca45de73e
Weat-ctrl <aaaaaaarrrrrrrgghhh@gmail.com>
2025-07-21 09:51:38 +0300

    Update script.js ?dts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.160.0';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.160.0/examples/jsm/controls/OrbitControls.js?dts';
 
 // --- Scene Setup ---
 const scene = new THREE.Scene();
