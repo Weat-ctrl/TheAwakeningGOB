f72cae10f8c3b98343d26e9bde19584f7f7b1571
Weat-ctrl <aaaaaaarrrrrrrgghhh@gmail.com>
2025-07-21 10:03:13 +0300

    Update script.js unpkg

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.160.0';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.160.0/examples/jsm/controls/OrbitControls.js?dts';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 
 // --- Scene Setup ---
 const scene = new THREE.Scene();
